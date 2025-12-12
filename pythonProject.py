34e3dd4a55ec857cc36f1d456d71ed7b18c299cb
Gianella Robles <gianellarobles@gmail.com>
2025-12-11 22:03:22 -0500

    Updated main code file

--- pythonProject.py
+++ pythonProject.py
@@ -7,6 +7,8 @@ Created on Wed Dec 10 19:38:44 2025
 """
 
 import pandas as pd
+import numpy as np
+import re
 
 f = pd.read_csv("/Users/gianellarobles/Downloads/movies.csv")
 
@@ -92,6 +94,138 @@ rt = rt.reset_index(drop=True)
 f = f.reset_index(drop=True)
 
 
+#check for any unknown values
+(rt == "Unknown").sum()
+
+#check the row 
+rt[rt.eq("Unknown").any(axis=1)]
+
+(f == "Unknown").sum()
+
+#drop the unknown
+rt = rt[rt["movie_title"] != "Unknown"]
+f = f[(f["Budget"] != "Unknown") & (f["Income"] != "Unknown")]
+
+#Convert to USD 
+exchange_rates = {
+    '$': 1,          
+    '€': 1.08,        
+    '£': 1.27,           
+    '₹': 0.012,        
+    '₩': 0.00074,
+    '¥': 0.0067,         
+    'CN¥': 0.14,         
+    'A$': 0.66,           
+    'CA$': 0.74,          
+    'SEK': 0.095,        
+    'DKK': 0.15,          
+    'NOK': 0.095,         
+}
+
+
+symbols = sorted(exchange_rates.keys(), key=len, reverse=True)
+
+
+#function about the aligning currency
+def parse_currency(value):
+    if pd.isna(value):
+        return pd.Series([np.nan, np.nan])
+
+    s = str(value).strip()
+
+    currency = np.nan
+    for sym in symbols:
+        if s.startswith(sym):
+            currency = sym
+            s = s[len(sym):].strip() 
+            break
+
+    if pd.isna(currency):
+        return pd.Series([np.nan, np.nan])
+
+    num_str = re.sub(r'[^0-9.]', '', s)
+    if num_str == '':
+        amount = np.nan
+    else:
+        amount = float(num_str)
+
+    return pd.Series([currency, amount])
+
+
+f[['currency', 'amount']] = f['Budget'].apply(parse_currency)
+
+f['Budget_USD'] = f.apply(
+    lambda row: row['amount'] * exchange_rates.get(row['currency'], np.nan),
+    axis=1
+)
+
+#view the first 20 of the new columns
+
+f[['Budget', 'currency', 'amount', 'Budget_USD']].head(20)
+f[['income_currency', 'income_amount']] = f['Income'].apply(parse_currency)
+
+
+f['Income_USD'] = f.apply(
+    lambda row: row['income_amount'] * exchange_rates.get(row['income_currency'], np.nan),
+    axis=1
+)
+
+#Compute profit and ROI
+f['Profit_USD'] = f['Income_USD'] - f['Budget_USD']
+
+#ROI(Return On Investment) - how much profit you made compared to how much you invested.
+f['ROI'] = f['Profit_USD'] / f['Budget_USD']
+f['ROI_pct'] = f['ROI'] * 100
+
+
+#getting ready to merge
+f['title_id'] = f['Title'].astype(str).str.strip().str.lower()
+rt['id'] = rt['movie_title'].astype(str).str.strip().str.lower()
+
+
+#take away duplicates
+rt_sorted = rt.sort_values('year')
+
+rt_unique = rt_sorted.drop_duplicates(
+
+    subset=['movie_title', 'year'],
+    keep='first'
+)
+
+rt_filtered = rt_unique[
+    [ 'movie_title','year', 'tomatometer_status', 'tomatometer_rating']
+]
+
+
+#mergeing using left (movies.csv) and keys ('Title' and 'Year')
+f['year'] = f['Year']          
+rt['year'] = pd.to_datetime(rt['year'])
+
+#merged - new dataset combining both csv files
+merged = f.merge(
+    rt_filtered,
+    how='left',
+    left_on=['Title', 'year'],
+    right_on=['movie_title', 'year']
+)
+
+#1646 rows
+print(len(f), len(merged))
+
+
+#Numnber of rows with a non-missing rating
+rating_not_missing = len(merged) - merged['tomatometer_rating'].notna().sum()
+print(rating_not_missing)
+#483
+
+
+#creating a list within the column 'Genre'
+f['Genre_list'] = f['Genre'].str.split(',').apply(lambda x: [g.strip() for g in x])
+
+f_exploded = f.explode('Genre_list')
+
+
+
 #rename columns
 rt = rt.rename(columns={
     "movie_title": "Title",
@@ -103,6 +237,8 @@ rt = rt.rename(columns={
     
     
     })
+
+
 
 
 
